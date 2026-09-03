a6f099c107358f077cdeaab735a0e94b4c620460
luminarsoundwrx <felipearevalomusic@gmail.com>
2026-09-03 14:40:10 +0100

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,11 @@
+window.WMT_CONFIG = {
+  siteName: 'Web Music Tools',
+  gaMeasurementId: '',
+  adsenseClient: '',
+  adSlots: {
+    leftRail: '',
+    rightRail: '',
+    inline: '',
+    mobileAnchor: ''
+  }
+};
